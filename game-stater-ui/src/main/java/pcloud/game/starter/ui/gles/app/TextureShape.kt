c5b143c4a9b4603dd044c6f4a0f728dd2871ab90
PCloud <pcloud63514@gmail.com>
2022-11-05 21:03:53 +0900

    refactor: 늘어짐 해결 실패 ㅂㄷㅂㄷ

--- game-stater-ui/src/main/java/pcloud/game/starter/ui/gles/app/TextureShape.kt
+++ game-stater-ui/src/main/java/pcloud/game/starter/ui/gles/app/TextureShape.kt
@@ -4,6 +4,7 @@ import android.opengl.GLUtils
 import pcloud.game.starter.ui.gles.model.*
 import java.nio.FloatBuffer
 import javax.microedition.khronos.opengles.GL10
+import javax.microedition.khronos.opengles.GL10.GL_TRIANGLES
 
 class TextureShape {
     private var textureHandle = -1
@@ -29,6 +30,8 @@ class TextureShape {
         gl.glClearColor(0f, 0f, 0f, 0f)
         gl.glClear(GL10.GL_COLOR_BUFFER_BIT or GL10.GL_DEPTH_BUFFER_BIT)
         gl.glMatrixMode(GL10.GL_TEXTURE)
+//        gl.glMatrixMode(GL10.GL_MODELVIEW)
+//        gl.glLoadIdentity()
         gl.glEnableClientState(GL10.GL_VERTEX_ARRAY)
         gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY)
         gl.glEnable(GL10.GL_DEPTH_TEST)
@@ -36,16 +39,23 @@ class TextureShape {
         gl.glEnable(GL10.GL_TEXTURE_2D)
         gl.glVertexPointer(3, GL10.GL_FLOAT, 0, VERTEX_BUFFER)
         gl.glBindTexture(GL10.GL_TEXTURE_2D, textureHandle)
+
+
+//        gl.glScalef(0.12f, 0.05f, 0f)
     }
 
     fun draw(gl: GL10, methodInfo:MethodInfo) {
         val translateX = (methodInfo.x - viewPointX).toInt() / textureWidth
         val translateY = (methodInfo.y - viewPointY).toInt() / textureHeight
 
-        gl.glDrawElements(GL10.GL_TRIANGLES, DRAW_ORDER.size, GL10.GL_UNSIGNED_SHORT, DRAW_ORDER_BUFFER)
-        gl.glTranslatef(translateX, translateY, 0f)
+        gl.glDrawElements(GL_TRIANGLES, DRAW_ORDER.size, GL10.GL_UNSIGNED_SHORT, DRAW_ORDER_BUFFER)
+        /**렌더링할 때 사용할 텍스처 좌표배열의 위치와 데이터를 지정하는 함수*/
         gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, getLookTextureBuffer(methodInfo))
 
+//        GLU.gluLookAt(gl, 00f, 0f, 0f, 10f, 10f, 0f, 0f, 0f, 0f)
+
+        /**정점 이동*/
+        gl.glTranslatef(translateX, translateY, 0f)
         viewPointX = methodInfo.x
         viewPointY = methodInfo.y
     }
@@ -60,9 +70,9 @@ class TextureShape {
         gl.glDisableClientState(GL10.GL_TEXTURE_COORD_ARRAY)
     }
 
-    private fun getLookTextureBuffer(methodInfo:MethodInfo): FloatBuffer {
-        val w = methodInfo.w / textureWidth
-        val h = methodInfo.h / textureHeight
+    private fun getLookTextureBuffer(methodInfo:MethodInfo?): FloatBuffer {
+        val w = 1f
+        val h = 1f
 
         val lookTexture = floatArrayOf(
             0.0f, 0.0f,
